e9d4e548f37d655afd7fe0369ec2ac0e640a480c
lumijoe <spacedesign63@gmail.com>
2024-06-08 17:43:25 +0900

    TEST

--- confirm.php
+++ confirm.php
@@ -4,3 +4,27 @@ Template Name: Confirm Page
 */
 get_header();
 ?>
+<?php echo do_shortcode('[]'); ?>
+<section>
+    <div class="contact_header brandcolor_bg">
+        <div>
+            <h1 class="title_03">お申し込みフォーム</h1>
+        </div>
+    </div>
+    <!-- パンくずリスト -->
+    <nav class="flex mt-2 ml-4" aria-label="Breadcrumb">
+    <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+        <li class="inline-flex items-center">
+            <a href="<?php echo home_url(); ?>" class="inline-flex items-center text-sm hover:text-blue-600 dark:hover:text-white">
+            Top
+            </a>
+        </li>
+        <li aria-current="page">
+            <div class="flex items-center">
+            >
+            <span class="ms-1 text-sm">お申し込み</span>
+            </div>
+        </li>
+        </ol>
+    </nav>
+</section>
